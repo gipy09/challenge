3dde945b99f6f0d6491ff822c76b36b74d46ce09
gipy09 <ricardoalvarez_09@hotmail.es>
2022-07-13 23:20:30 -0300

    change text

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -7,7 +7,7 @@ function Banner() {
         <div className="capa"></div>
         <div className="info">
           <h2>Su viaje de <strong>salud emocional</strong> empieza aqui</h2>
-          <h2>Conoce a nuestros profecionales con licencia</h2>
+          <h2>Conoce a nuestros profesionales con licencia</h2>
           <p>Todos se someten a un proceso seguro de verificacion y acreditacion,
             ademas de adherirse a estrictos codigos de etica y confidencialidad.
           </p>
